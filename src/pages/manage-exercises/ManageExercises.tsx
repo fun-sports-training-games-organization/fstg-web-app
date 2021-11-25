0b508d6b72ec460da5770bb78df16be0c18f423c
albertmarsnik <albertmarsnik@gmail.com>
2021-11-25 19:36:58 +0100

    #54: add exercises title, remove unused folder

--- src/pages/manage-exercises/ManageExercises.tsx
+++ src/pages/manage-exercises/ManageExercises.tsx
@@ -1,4 +1,4 @@
-import { Grid, IconButton, List, ListItem, Stack } from '@mui/material';
+import { Grid, IconButton, List, ListItem, Stack, Typography } from '@mui/material';
 import { Delete, Edit } from '@mui/icons-material';
 import { FC, useEffect, useState } from 'react';
 import { useFirestore } from 'react-redux-firebase';
@@ -42,48 +42,59 @@ const ManageExercises: FC = (): JSX.Element => {
     };
 
     return (
-        <Stack ml={2} mr={2} mt={3}>
-            <AddButton onClick={() => setOpenDialog(true)} testId={`${idPrefix}add_exercise_button`} />
-            <List>
-                {exercises.map((exercise: Exercise) => {
-                    return (
-                        <Grid container key={exercise.id} display={'flex'} flexDirection={'row'}>
-                            <Grid item xs={8}>
-                                <ListItem key={exercise.id}>{exercise.name}</ListItem>
+        <>
+            <Stack ml={2} mr={2} mt={3}>
+                <Grid container direction="row" justifyContent="center" alignItems="center">
+                    <Grid item xs={9}>
+                        <Typography variant="h4">Exercises</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <AddButton onClick={() => setOpenDialog(true)} testId={`${idPrefix}add_exercise_button`} />
+                    </Grid>
+                </Grid>
+            </Stack>
+            <Stack ml={2} mr={2} mt={3} mb={3}>
+                <List>
+                    {exercises.map((exercise: Exercise) => {
+                        return (
+                            <Grid container key={exercise.id} display={'flex'} flexDirection={'row'}>
+                                <Grid item xs={8}>
+                                    <ListItem key={exercise.id}>{exercise.name}</ListItem>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <IconButton onClick={() => handleUpdate(exercise)}>
+                                        <Edit htmlColor={'steelblue'} />
+                                    </IconButton>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <IconButton
+                                        onClick={() => {
+                                            handleDelete(exercise);
+                                        }}
+                                    >
+                                        <Delete htmlColor={'palevioletred'} />
+                                    </IconButton>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={2}>
-                                <IconButton onClick={() => handleUpdate(exercise)}>
-                                    <Edit htmlColor={'steelblue'} />
-                                </IconButton>
-                            </Grid>
-                            <Grid item xs={2}>
-                                <IconButton
-                                    onClick={() => {
-                                        handleDelete(exercise);
-                                    }}
-                                >
-                                    <Delete htmlColor={'palevioletred'} />
-                                </IconButton>
-                            </Grid>
-                        </Grid>
-                    );
-                })}
-            </List>
-            <EditExerciseDialog
-                title={t('dialog.editExercise.title')}
-                message={t('dialog.editExercise.message')}
-                open={openDialog}
-                setOpen={setOpenDialog}
-                exercise={exercise}
-                setExercise={setExercise}
-            />
-            <DeleteConfirmationDialog
-                openDeleteConfirmationDialog={openDeleteConfirmationDialog}
-                itemToDelete={exerciseToDelete}
-                collection="exercises"
-                closeDialog={() => setOpenDeleteConfirmationDialog(false)}
-            />
-        </Stack>
+                        );
+                    })}
+                </List>
+                <EditExerciseDialog
+                    title={t('dialog.editExercise.title')}
+                    message={t('dialog.editExercise.message')}
+                    open={openDialog}
+                    setOpen={setOpenDialog}
+                    exercise={exercise}
+                    setExercise={setExercise}
+                />
+                <DeleteConfirmationDialog
+                    openDeleteConfirmationDialog={openDeleteConfirmationDialog}
+                    itemToDelete={exerciseToDelete}
+                    collection="exercises"
+                    closeDialog={() => setOpenDeleteConfirmationDialog(false)}
+                />
+            </Stack>
+        </>
     );
 };
 export default ManageExercises;
