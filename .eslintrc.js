14651529810dda0a2f693d71fff59deb1cec18e2
hoj <jerry.howe@nimbus.ch>
2021-12-03 08:41:58 +0100

    Revert "updated story book to 6.4.0"
    This reverts commit 01ab75978f3ad3b61b6a9048d0ec893e8ab391f2.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,51 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['unused-imports'],
-  extends: ["react-app", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
-  parserOptions: {
-    ecmaVersion: 2020,
-    // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
-
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    curly: 'error',
-    semi: 'error',
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 2,
-    'unused-imports/no-unused-vars': 1,
-    camelcase: 'off',
-    'prettier/prettier': ['warn', {
-      endOfLine: 'auto'
-    }],
-    '@typescript-eslint/camelcase': ['off'],
-    // turn camelcase checking off for now...
-    'no-use-before-define': 'off',
-    // https://stackoverflow.com/a/64024916/1892214
-    '@typescript-eslint/no-use-before-define': ['error']
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  overrides: [{
-    files: ['*.test.ts', '*.test.tsx'],
-    globals: {
-      shallow: true,
-      render: true,
-      mount: true,
-      React: true,
-      fetchMock: true,
-      i18n: true
-    }
-  }]
+    parser: '@typescript-eslint/parser',
+    plugins: ['unused-imports'],
+    extends: [
+        'react-app',
+        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last config in the extends array.
+    ],
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module' // Allows for the use of imports
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        curly: 'error',
+        semi: 'error',
+        'react/prop-types': 'off',
+        'no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 2,
+        'unused-imports/no-unused-vars': 1,
+        camelcase: 'off',
+        'prettier/prettier': [
+            'warn',
+            {
+                endOfLine: 'auto'
+            }
+        ],
+        '@typescript-eslint/camelcase': ['off'], // turn camelcase checking off for now...
+        'no-use-before-define': 'off', // https://stackoverflow.com/a/64024916/1892214
+        '@typescript-eslint/no-use-before-define': ['error']
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    overrides: [
+        {
+            files: ['*.test.ts', '*.test.tsx'],
+            globals: {
+                shallow: true,
+                render: true,
+                mount: true,
+                React: true,
+                fetchMock: true,
+                i18n: true
+            }
+        }
+    ]
 };
