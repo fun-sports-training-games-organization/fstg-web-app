19d41c2dfa103ccedc9b36d5617775759c8d1754
albertmarsnik <albertmarsnik@gmail.com>
2021-12-15 15:54:31 +0100

    implement accordion and 3 dot menu on manage workouts page, visually still looks weird

--- src/components/molecules/accordion/Accordion.types.ts
+++ src/components/molecules/accordion/Accordion.types.ts
@@ -1,13 +1,13 @@
 import { AccordionProps as MUIAccordionProps } from '@mui/material';
 import { ReactNode } from 'react';
 
-interface Accordion {
+export interface AccordionProp {
     title: string | ReactNode;
     subtitle?: string | ReactNode;
     content: string | ReactNode;
 }
 export interface CustomProps {
-    accordions: Accordion[];
+    accordions: AccordionProp[];
 }
 
 export type AccordionProps = CustomProps & Omit<MUIAccordionProps, 'children'>;
--- src/pages/workouts/manage/ManageWorkouts.tsx
+++ src/pages/workouts/manage/ManageWorkouts.tsx
@@ -1,18 +1,24 @@
 import { FC, useEffect, useState } from 'react';
-import { Grid, IconButton, List, ListItem, Stack } from '@mui/material';
+import { Grid, IconButton, List, MenuItem, MenuList, Stack, Typography } from '@mui/material';
 import { useFirestore } from 'react-redux-firebase';
 import { Delete, Edit } from '@mui/icons-material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useHistory } from 'react-router-dom';
 import DeleteConfirmationDialog from '../../../components/molecules/delete-confirmation-dialog/DeleteConfirmationDialog';
 import PageTitleAdd from '../../../components/molecules/page-title-add/PageTitleAdd';
-import WorkoutExercises from '../../../components/organisms/workout-exercises/WorkoutExercises';
+// import WorkoutExercises from '../../../components/organisms/workout-exercises/WorkoutExercises';
 import { Workout } from '../../../model/workout';
 import { getPageIdPrefix } from '../../../util/id-util';
 import * as navigate from '../../../util/navigation-util';
+import Accordion from '../../../components/molecules/accordion/Accordion';
+import { AccordionProp } from '../../../components/molecules/accordion/Accordion.types';
+import { ExerciseWorkoutSettings } from '../../../model/exercise-workout-settings';
+import MenuIcon from '../../../components/atoms/menu-icon/MenuIcon';
 
 const ManageWorkouts: FC = () => {
     const pageName = 'manage_workouts';
     const idPrefix = getPageIdPrefix(pageName);
+    const exerciseItemPrefix = `${idPrefix}exercise_list__item_`;
     const history = useHistory();
     const firestore = useFirestore();
 
@@ -35,6 +41,25 @@ const ManageWorkouts: FC = () => {
         });
     }, [firestore]);
 
+    const getAccordionProp = (workout: Workout, exerciseItemPrefix: string, index: number): AccordionProp[] => {
+        const getExerciseItem = (exercise: ExerciseWorkoutSettings) => {
+            return (
+                <Typography key={exercise.id} id={`${exerciseItemPrefix}${index}`} align="left" variant="body2">
+                    {exercise.name}
+                </Typography>
+            );
+        };
+
+        const getAccordionContent = workout.exercises.map((exercise) => getExerciseItem(exercise));
+
+        return [
+            {
+                title: workout.name,
+                content: getAccordionContent
+            }
+        ];
+    };
+
     return (
         <div data-testid={pageName}>
             <PageTitleAdd
@@ -47,27 +72,44 @@ const ManageWorkouts: FC = () => {
                     {workouts.map((workout: Workout, index: number) => {
                         return (
                             <Grid container key={workout.id} display={'flex'} flexDirection={'row'}>
-                                <Grid item xs={8}>
-                                    <ListItem key={workout.id}>{workout.name}</ListItem>
-                                    <WorkoutExercises parentIdPrefix={idPrefix} workout={workout} />
+                                <Grid item xs={11}>
+                                    {/* <ListItem key={workout.id}>{workout.name}</ListItem>
+                                    <WorkoutExercises parentIdPrefix={idPrefix} workout={workout} /> */}
+                                    <Accordion
+                                        accordions={getAccordionProp(workout, exerciseItemPrefix, index)}
+                                    ></Accordion>
                                 </Grid>
-                                <Grid item xs={2}>
-                                    <IconButton
-                                        data-testid={`${idPrefix}edit_item_${index}`}
-                                        onClick={() => navigate.toEditWorkout(history, workout.id)}
-                                    >
-                                        <Edit htmlColor={'steelblue'} />
-                                    </IconButton>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <IconButton
-                                        data-testid={`${idPrefix}delete_item_${index}`}
-                                        onClick={() => {
-                                            handleDelete(workout);
-                                        }}
-                                    >
-                                        <Delete htmlColor={'palevioletred'} />
-                                    </IconButton>
+                                <Grid item xs={1}>
+                                    <MenuIcon icon={<MoreVertIcon style={{ color: 'black' }} />}>
+                                        <MenuList>
+                                            <MenuItem
+                                                key={`${idPrefix}edit_item_${index}`}
+                                                onClick={() => navigate.toEditWorkout(history, workout.id)}
+                                            >
+                                                <IconButton
+                                                    data-testid={`${idPrefix}edit_item_${index}`}
+                                                    onClick={() => navigate.toEditWorkout(history, workout.id)}
+                                                >
+                                                    <Edit htmlColor={'steelblue'} />
+                                                </IconButton>
+                                            </MenuItem>
+                                            <MenuItem
+                                                key={`${idPrefix}delete_item_${index}`}
+                                                onClick={() => {
+                                                    handleDelete(workout);
+                                                }}
+                                            >
+                                                <IconButton
+                                                    data-testid={`${idPrefix}delete_item_${index}`}
+                                                    onClick={() => {
+                                                        handleDelete(workout);
+                                                    }}
+                                                >
+                                                    <Delete htmlColor={'palevioletred'} />
+                                                </IconButton>
+                                            </MenuItem>
+                                        </MenuList>
+                                    </MenuIcon>
                                 </Grid>
                             </Grid>
                         );
