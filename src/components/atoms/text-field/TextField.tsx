d6196934a6dfe1776513b83e156583cf2f7f431d
hoj <jerry.howe@nimbus.ch>
2021-12-18 11:30:10 +0100

    added some comments

--- src/components/atoms/text-field/TextField.tsx
+++ src/components/atoms/text-field/TextField.tsx
@@ -2,9 +2,9 @@ import React, { FC } from 'react';
 import { FormControl, FormHelperText, InputLabel, OutlinedInput, OutlinedInputProps } from '@mui/material';
 
 type OwnProps = {
-    error?: boolean;
-    helperText?: string;
-    shrinkLabel?: boolean;
+    helperText?: string; // this is text that shows underneath the outlined input
+    error?: boolean; // if true, the text underneath (if any) will show in 'error' color
+    shrinkLabel?: boolean; // this forces the label to shrink and sit on the top left of the text field and creates a notch in the outlined input as well
 };
 
 export type TextFieldProps = OwnProps & OutlinedInputProps;
