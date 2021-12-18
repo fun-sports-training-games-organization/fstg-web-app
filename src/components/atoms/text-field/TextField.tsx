cbc7c4cbe4189262d30755073edd7c8cbab78bf4
Jerry Howe <jerry.howe@gmail.com>
2021-12-18 15:29:33 +0100

    Feature/file chooser (#97)
    * - added FileChooser
    - added shrinkLabel param in TextField to shrink label manually because of the overlapping label over choose file button
    
    * added some comments
    
    Co-authored-by: hoj <jerry.howe@nimbus.ch>

--- src/components/atoms/text-field/TextField.tsx
+++ src/components/atoms/text-field/TextField.tsx
@@ -2,17 +2,20 @@ import React, { FC } from 'react';
 import { FormControl, FormHelperText, InputLabel, OutlinedInput, OutlinedInputProps } from '@mui/material';
 
 type OwnProps = {
-    error?: boolean;
-    helperText?: string;
+    helperText?: string; // this is text that shows underneath the outlined input
+    error?: boolean; // if true, the text underneath (if any) will show in 'error' color
+    shrinkLabel?: boolean; // this forces the label to shrink and sit on the top left of the text field and creates a notch in the outlined input as well
 };
 
 export type TextFieldProps = OwnProps & OutlinedInputProps;
 
-const TextField: FC<TextFieldProps> = ({ id, label, helperText, error, ...rest }: TextFieldProps) => {
+const TextField: FC<TextFieldProps> = ({ id, label, helperText, shrinkLabel, error, ...rest }: TextFieldProps) => {
     return (
         <FormControl fullWidth>
-            <InputLabel htmlFor={id}>{label}</InputLabel>
-            <OutlinedInput id={id} label={label} {...rest} />
+            <InputLabel shrink={shrinkLabel} htmlFor={id}>
+                {label}
+            </InputLabel>
+            <OutlinedInput notched={shrinkLabel} id={id} label={label} {...rest} />
             <FormHelperText error={error}>{helperText}</FormHelperText>
         </FormControl>
     );
