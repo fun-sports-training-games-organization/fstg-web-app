9fd9077577e07913a57f22db78278141e79a81b9
albertmarsnik <albertmarsnik@gmail.com>
2022-01-11 15:57:14 +0100

    remove not implemented tooltip

--- src/components/templates/cards/record-card/RecordCardItem.tsx
+++ src/components/templates/cards/record-card/RecordCardItem.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { FC } from 'react';
-import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText, Tooltip, Typography } from '@mui/material';
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText, /* Tooltip, */ Typography } from '@mui/material';
 import { WorkoutResult } from '../../../../model/Workout.model';
 import { useTranslation } from 'react-i18next';
 import { formatSecondsValueInHoursMinutesAndSeconds } from '../../../../util/date-util';
@@ -27,21 +27,21 @@ const RecordCardItem: FC<WorkoutCardItemProps> = ({ workoutResult }: WorkoutCard
     return (
         <>
             <ListItem key={workoutResult.id} disablePadding sx={{ marginBottom: 1 }}>
-                <Tooltip arrow title={t('global.notYetImplemented') as string}>
-                    <ListItemButton onClick={() => setIsNotYetImplementedDialogOpen(true)}>
-                        <Grid container alignItems={'center'}>
-                            <Grid item xs={12} sm={6}>
-                                <ListItemText primary={`${name}${t('global.colon')}`} />
-                            </Grid>
-                            <Grid item xs={12} sm={6} display="flex" flexDirection="row" justifyContent="flex-start">
-                                <ListItemIcon sx={{ minWidth: '2rem' }}>
-                                    {resultType === 'COUNT_BASED' ? <NumbersIcon /> : <AccessTimeIcon />}
-                                </ListItemIcon>
-                                <Typography>{formattedValue}</Typography>
-                            </Grid>
+                {/* <Tooltip arrow title={t('global.notYetImplemented') as string}> */}
+                <ListItemButton onClick={() => setIsNotYetImplementedDialogOpen(true)}>
+                    <Grid container alignItems={'center'}>
+                        <Grid item xs={12} sm={6}>
+                            <ListItemText primary={`${name}${t('global.colon')}`} />
                         </Grid>
-                    </ListItemButton>
-                </Tooltip>
+                        <Grid item xs={12} sm={6} display="flex" flexDirection="row" justifyContent="flex-start">
+                            <ListItemIcon sx={{ minWidth: '2rem' }}>
+                                {resultType === 'COUNT_BASED' ? <NumbersIcon /> : <AccessTimeIcon />}
+                            </ListItemIcon>
+                            <Typography>{formattedValue}</Typography>
+                        </Grid>
+                    </Grid>
+                </ListItemButton>
+                {/* </Tooltip> */}
             </ListItem>
             <NotYetImplementedDialog
                 open={isNotYetImplementedDialogOpen}
